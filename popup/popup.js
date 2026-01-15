6367dae85655e0303ff94cc182eade1e02a1e697
gayathri-475 <gayathribalu475@gmail.com>
2026-01-15 13:53:51 +0530

    Add popup logic for toggle and start/stop reminders

---
+++ popup/popup.js
@@ -0,0 +1,62 @@
+const postureBtn = document.getElementById("postureBtn");
+const waterBtn = document.getElementById("waterBtn");
+
+const postureSection = document.getElementById("postureSection");
+const waterSection = document.getElementById("waterSection");
+
+const startPosture = document.getElementById("startPosture");
+const stopPosture = document.getElementById("stopPosture");
+
+const startWater = document.getElementById("startWater");
+const stopWater = document.getElementById("stopWater");
+
+const postureTime = document.getElementById("postureTime");
+const waterTime = document.getElementById("waterTime");
+
+postureBtn.addEventListener("click", () => {
+    postureBtn.classList.add("active");
+    waterBtn.classList.remove("active");
+
+    postureSection.classList.remove("hidden");
+    waterSection.classList.add("hidden");
+});
+
+waterBtn.addEventListener("click" , () => {
+    waterBtn.classList.add("active");
+    postureBtn.classList.remove("active");
+
+    waterSection.classList.remove("hidden");
+    postureSection.classList.add("hidden");
+});
+
+startPosture.addEventListener("click", () => {
+    const minutes = Number(postureTime.value);
+
+    chrome.runtime.sendMessage({
+        action: "START_REMINDER",
+        type: "posture",
+        interval: minutes
+    });
+});
+
+stopPosture.addEventListener("click", () => {
+    chrome.runtime.sendMessage({
+        action: "STOP_REMINDER",
+        type: "posture"
+    });
+});
+
+startWater.addEventListener("click", () => {
+    chrome.runtime.sendMessage({
+        action: "START_REMINDER",
+        type: "water",
+        interval: minutes
+    });
+});
+
+stopWater.addEventListener("click", () => {
+    chrome.runtime.sendMessage({
+        action: "STOP_REMINDER",
+        type: "water"
+    });
+});
